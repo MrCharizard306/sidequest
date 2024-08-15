68be729005e1a725d9d9e56ec3880d3eff37fa63
MrCharizard306 <qitaoshi@gmail.com>
2024-08-16 02:31:02 +1000

    updated main got ride of old

--- lib/main.dart
+++ lib/main.dart
@@ -17,39 +17,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< Updated upstream
-      home: Scaffold(
-        
-        backgroundColor: Colors.deepPurple,
-        body:  Center(
-          child: GestureDetector(
-            onTap: (){
-              print("hello");
-            },
-            child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(255, 160, 35, 100),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(125),
-              child: Text(
-                "SIDEQUEST",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
-              ),
-            
-            
-            ),
-          ),
-        ),
-      ),
-=======
       home: LoginPage(),
->>>>>>> Stashed changes
     );
   }
 }
